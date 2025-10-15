2b7128cac3b20b3867d63cfac5d019b66ce74bbe
kaylorchen <kaylor.chen@qq.com>
2025-10-15 12:46:30 +0800

    Update

--- install_ros2_compilation_tools.sh
+++ install_ros2_compilation_tools.sh
@@ -2,3 +2,5 @@
 apt update
 apt install -y fakeroot debhelper devscripts ros-dev-tools python3-bloom
 cp -v build_ros2_deb /usr/local/bin/
+cp -v build_ros2_deb /usr/local/bin/build_ros2_deb_with_clean
+echo "fakeroot debian/rules clean" | tee -a /usr/local/bin/build_ros2_deb_with_clean
